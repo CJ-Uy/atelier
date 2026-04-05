02279ecd33570e95b7ffc32904646bcd1ac5e812
CJ-Uy <75460325+CJ-Uy@users.noreply.github.com>
2026-04-05 13:11:21 +0800

    feat(test): configure Vitest and Playwright

---
+++ vitest.config.ts
@@ -0,0 +1,12 @@
+// vitest.config.ts
+import { defineConfig } from 'vitest/config';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+
+export default defineConfig({
+  plugins: [svelte({ hot: !process.env.VITEST })],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    include: ['src/**/__tests__/**/*.test.ts'],
+  },
+});
